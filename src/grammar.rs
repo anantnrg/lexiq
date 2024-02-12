b6f6bbd452a56e4dc12dc06d9c22765fe164cd6e
Anant Narayan <anantnrg@gmail.com>
2024-02-12 17:25:15 +0530

    Parse yaml into struct

--- src/grammar.rs
+++ src/grammar.rs
@@ -1,18 +1,39 @@
-#[derive(Debug, Clone)]
+use std::{fs::File, io::Read, path::PathBuf};
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone)]
 pub struct Grammar {
     pub name: String,
     pub extensions: Vec<String>,
     pub groups: Vec<Group>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Group {
     pub name: String,
     pub rules: Vec<Rule>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Rule {
     pub regex: String,
     pub scope: String,
+}
+
+impl Grammar {
+    pub fn new(name: String, grammar: PathBuf) -> Self {
+        let mut file = String::new();
+        File::open(grammar)
+            .expect("Couldn't find the specified grammar file")
+            .read_to_string(&mut file)
+            .expect("Couldn't parse the given file");
+
+        let grammar: Grammar = serde_yaml::from_str(&file)
+            .expect("Failed to parse into struct Grammar. Is the file a YAML file?");
+
+        println!("{:?}", grammar);
+
+        grammar
+    }
 }
