b96df86cbef0f2e222c0f32a783437ebbbe940e9
Anant Narayan <anantnrg@gmail.com>
2024-02-14 20:23:48 +0530

    Add constants

--- src/grammars/rust.rs
+++ src/grammars/rust.rs
@@ -2,16 +2,18 @@ use crate::grammar::{Grammar, Rule, Syntax};
 
 pub struct RustLang;
 
-const NonRawIndentifier: &'static str = "";
-const Identifier: &'static str = "";
-const CamelCaseIdentifier: &'static str = "";
-const Lifetime: &'static str = "";
-const EscapedByte: &'static str = "";
-const IntegerSuffixes: &'static str = "";
-const FloatSuffixes: &'static str = "";
-const FloatExponent: &'static str = "";
-const DecimalLiteral: &'static str = "";
-const TypeIdentifier: &'static str = "";
+const NON_RAW_INDENTIFIER: &'static str = r#"[[:alpha:]][_[:alnum:]]*|_[_[:alnum:]]+"#;
+const IDENTIFIER: &'static str = r#"(?:(?:(?:r\#)?{{non_raw_ident}})\b)"#;
+const CAMEL_CASE_IDENTIFIER: &'static str = r#"\b_*[A-Z][a-zA-Z0-9_]*[a-z][a-zA-Z0-9_]*\b"#;
+const LIFETIME: &'static str = r#"'(?:_|{{non_raw_ident}})(?!\')\b"#;
+const ESCAPED_BYTE: &'static str = r#"\\([nrt0\"'\\]|x\h{2})"#;
+const ESCAPED_CHAR: &'static str = r#"\\([nrt0\"''\\]|x[0-7]\h|u\{(?:\h_*){1,6}\})"#;
+const INTEGER_SUFFIXES: &'static str = r#"[iu](?:8|16|32|64|128|size)"#;
+const FLOAT_SUFFIXES: &'static str = r#"f(32|64)"#;
+const FLOAT_EXPONENT: &'static str = r#"[eE][+-]?[0-9_]*[0-9][0-9_]*"#;
+const DECIMAL_LITERAL: &'static str = r#"[0-9](?:[0-9_])*"#;
+const TYPE_IDENTIFIER: &'static str =
+    r#"\b(?:[[:upper:]]|_*[[:upper:]][[:alnum:]_]*[[:lower:]][[:alnum:]_]*)\b"#;
 
 impl RustLang {
     pub fn grammar() -> Grammar {
