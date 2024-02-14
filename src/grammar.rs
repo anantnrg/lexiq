3dc78fb11c48407dd81814861c6ca25acf200b32
Anant Narayan <anantnrg@gmail.com>
2024-02-14 18:31:32 +0530

    New new() method hehe

--- src/grammar.rs
+++ src/grammar.rs
@@ -32,19 +32,12 @@ pub struct Match {
 }
 
 impl Grammar {
-    pub fn new(grammar: PathBuf) -> Self {
-        let mut file = String::new();
-        File::open(grammar)
-            .expect("Couldn't find the specified grammar file")
-            .read_to_string(&mut file)
-            .expect("Couldn't parse the given file");
-
-        let grammar = serde_yaml::from_str(&file)
-            .expect("Failed to parse into struct Grammar. Is the file a YAML file?");
-
-        println!("{:?}", grammar);
-
-        grammar
+    pub fn new(name: String, extensions: Vec<String>, syntax: Syntax) -> Self {
+        Grammar {
+            name,
+            extensions,
+            syntax,
+        }
     }
 
     pub fn get_comments(&self, code: String) -> Vec<Match> {
