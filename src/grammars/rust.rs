03a6ca195c9d3931ed6ff8733ba8e8c4a4497eca
Anant Narayan <anantnrg@gmail.com>
2024-02-15 08:14:15 +0530

    Remove constants because useless

--- src/grammars/rust.rs
+++ src/grammars/rust.rs
@@ -3,19 +3,6 @@ use crate::rule;
 
 pub struct RustLang;
 
-const IDENTIFIER: &'static str = r#"\b[a-z_][a-z0-9_]*\b"#;
-const CAMEL_CASE_IDENTIFIER: &'static str = r#"\b[A-Z][a-z0-9]*([A-Z][a-z0-9]*)*\b
-"#;
-const LIFETIME: &'static str = r#"'(?:_|[[:alpha:]][_[:alnum:]]*|_[_[:alnum:]]+)(?!\')\b"#;
-const ESCAPED_BYTE: &'static str = r#"\\([nrt0\"'\\]|x\h{2})"#;
-const ESCAPED_CHAR: &'static str = r#"\\([nrt0\"''\\]|x[0-7]\h|u\{(?:\h_*){1,6}\})"#;
-const INTEGER_SUFFIXES: &'static str = r#"[iu](?:8|16|32|64|128|size)"#;
-const FLOAT_SUFFIXES: &'static str = r#"f(32|64)"#;
-const FLOAT_EXPONENT: &'static str = r#"[eE][+-]?[0-9_]*[0-9][0-9_]*"#;
-const DECIMAL_LITERAL: &'static str = r#"[0-9](?:[0-9_])*"#;
-const TYPE_IDENTIFIER: &'static str =
-    r#"\b(?:[[:upper:]]|_*[[:upper:]][[:alnum:]_]*[[:lower:]][[:alnum:]_]*)\b"#;
-
 impl RustLang {
     pub fn grammar() -> Grammar {
         Grammar {
