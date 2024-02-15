ee13d218e01e339b1f48617ef2cad3807b438eb6
Anant Narayan <anantnrg@gmail.com>
2024-02-15 10:23:24 +0530

    Add precendence fields when creating rules

--- src/grammars/rust.rs
+++ src/grammars/rust.rs
@@ -6,9 +6,11 @@ pub struct RustLang;
 const COMMENTS: usize = 0;
 const STRINGS: usize = 1;
 const KEYWORDS: usize = 2;
-const IDENTIFIERS: usize = 3;
-const NUMBERS: usize = 4;
-const PUNCTUATION: usize = 5;
+const TYPES: usize = 3;
+const LIFETIMES: usize = 4;
+const IDENTIFIERS: usize = 5;
+const NUMBERS: usize = 6;
+const PUNCTUATION: usize = 7;
 
 impl RustLang {
     pub fn grammar() -> Grammar {
@@ -25,15 +27,24 @@ impl RustLang {
     }
     pub fn keywords() -> Vec<Rule> {
         vec![
-            rule!(r#"\b[a-z_][a-z0-9_]*\b"#, "keywords.identifier"),
+            rule!(
+                r#"\b[a-z_][a-z0-9_]*\b"#,
+                "keywords.identifier",
+                IDENTIFIERS
+            ),
             rule!(
                 r#"\b[A-Z][a-z0-9]*([A-Z][a-z0-9]*)*\b"#,
-                "keywords.camelcase-ident"
+                "keywords.camelcase-ident",
+                IDENTIFIERS
             ),
-            rule!(r#"\b[A-Z0-9_]+\b"#, "keywords.upper-snakecase"),
-            rule!(r#"\b[iu](?:8|16|32|64|128|size)\b"#, "keywords.integer"),
-            rule!(r#"\bf(32|64)\b"#, "keywords.float"),
-            rule!(r#"\b[A-Z][a-zA-Z0-9_]*\b"#, "keywords.type"),
+            rule!(r#"\b[A-Z0-9_]+\b"#, "keywords.upper-snakecase", IDENTIFIERS),
+            rule!(
+                r#"\b[iu](?:8|16|32|64|128|size)\b"#,
+                "keywords.integer",
+                TYPES
+            ),
+            rule!(r#"\bf(32|64)\b"#, "keywords.float", TYPES),
+            rule!(r#"\b[A-Z][a-zA-Z0-9_]*\b"#, "keywords.type", TYPES),
         ]
     }
     pub fn punctuation() -> Vec<Rule> {
@@ -43,16 +54,17 @@ impl RustLang {
         vec![
             rule!(
                 r#"\b\d+(\_\d+)*([uUiIfF](8|16|32|64|size)?)?\b"#,
-                "data.decimal"
+                "data.decimal",
+                NUMBERS
             ),
-            rule!(r#"\b\d+(\.\d+)?[eE][+-]?\d+\b"#, "data.float"),
-            rule!(r#"'\w+"#, "data.lifetime"),
+            rule!(r#"\b\d+(\.\d+)?[eE][+-]?\d+\b"#, "data.float", NUMBERS),
+            rule!(r#"'\w+"#, "data.lifetime", LIFETIMES),
         ]
     }
     pub fn comments() -> Vec<Rule> {
         vec![
-            rule!(r#"\/\/.*"#, "comments.line"),
-            rule!(r#"\/\*[\s\S]*?\*\/"#, "comments.block"),
+            rule!(r#"\/\/.*"#, "comments.line", COMMENTS),
+            rule!(r#"\/\*[\s\S]*?\*\/"#, "comments.block", COMMENTS),
         ]
     }
 }
